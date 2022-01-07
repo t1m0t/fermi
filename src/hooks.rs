f073c344ed868e7060d69df9a702bbabe21acee2
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-01-07 15:39:49 -0500

    wip: improve hooks

--- src/hooks.rs
+++ src/hooks.rs
@@ -6,69 +6,57 @@ use crate::{AtomId, AtomRoot, Readable, Writable};
 
 // Initializes the atom root and retuns it;
 pub fn use_init_atom_root(cx: &ScopeState) -> &Rc<RefCell<AtomRoot>> {
-    cx.use_hook(
-        |_| {
-            if let Some(ctx) = cx.consume_state::<RefCell<AtomRoot>>() {
-                // panic!("there should not already be an atoms context");
-                ctx
-            } else {
-                let update_any = cx.schedule_update_any();
-                cx.provide_state(RefCell::new(AtomRoot::new(update_any)));
-                cx.consume_state::<RefCell<AtomRoot>>().unwrap()
-            }
-        },
-        |f| f,
-    )
+    cx.use_hook(|_| {
+        if let Some(ctx) = cx.consume_context::<RefCell<AtomRoot>>() {
+            // panic!("there should not already be an atoms context");
+            ctx
+        } else {
+            let update_any = cx.schedule_update_any();
+            cx.provide_context(RefCell::new(AtomRoot::new(update_any)));
+            cx.consume_context::<RefCell<AtomRoot>>().unwrap()
+        }
+    })
 }
 
 pub fn use_atom_root(cx: &ScopeState) -> &Rc<RefCell<AtomRoot>> {
-    cx.use_hook(
-        |_| cx.consume_state::<RefCell<AtomRoot>>().unwrap(),
-        |val| val,
-    )
+    cx.use_hook(|_| cx.consume_context::<RefCell<AtomRoot>>().unwrap())
 }
 
 pub fn use_read<'a, V: 'static>(cx: &'a ScopeState, f: impl Readable<V>) -> &'a V {
     let id = f.unique_id();
-    cx.use_hook(
-        |_| {
-            let root = cx.consume_state::<RefCell<AtomRoot>>().unwrap();
-            let scope_id = cx.scope_id();
-            UseReadInner {
-                value: None,
-                root,
-                id,
-                scope_id,
-            }
-        },
-        |inner| {
-            let value = inner.root.borrow_mut().register(f, cx.scope_id());
-            inner.value = Some(value);
-            inner.value.as_ref().unwrap()
-        },
-    )
+    let inner = cx.use_hook(|_| {
+        let root = cx.consume_context::<RefCell<AtomRoot>>().unwrap();
+        let scope_id = cx.scope_id();
+        UseReadInner {
+            value: None,
+            root,
+            id,
+            scope_id,
+        }
+    });
+
+    let value = inner.root.borrow_mut().register(f, cx.scope_id());
+    inner.value = Some(value);
+    inner.value.as_ref().unwrap()
 }
 
 /// Returns the RC of the value directly
 pub fn use_read_rc<'a, V: 'static>(cx: &'a ScopeState, f: impl Readable<V>) -> &'a Rc<V> {
     let id = f.unique_id();
-    cx.use_hook(
-        |_| {
-            let root = cx.consume_state::<RefCell<AtomRoot>>().unwrap();
-            let scope_id = cx.scope_id();
-            UseReadInner {
-                value: None,
-                root,
-                id,
-                scope_id,
-            }
-        },
-        |inner| {
-            let value = inner.root.borrow_mut().register(f, cx.scope_id());
-            inner.value = Some(value);
-            inner.value.as_ref().unwrap()
-        },
-    )
+    let inner = cx.use_hook(|_| {
+        let root = cx.consume_context::<RefCell<AtomRoot>>().unwrap();
+        let scope_id = cx.scope_id();
+        UseReadInner {
+            value: None,
+            root,
+            id,
+            scope_id,
+        }
+    });
+
+    let value = inner.root.borrow_mut().register(f, cx.scope_id());
+    inner.value = Some(value);
+    inner.value.as_ref().unwrap()
 }
 
 struct UseReadInner<V> {
@@ -107,22 +95,17 @@ impl<'a, V> Clone for UseReadWrite<'a, V> {
 
 pub fn use_set<'a, T: 'static>(cx: &'a ScopeState, f: impl Writable<T>) -> &'a Rc<dyn Fn(T)> {
     let root = use_atom_root(cx);
-    cx.use_hook(
-        |_| {
-            let id = f.unique_id();
-            let root = root.clone();
-            let root2 = root.clone();
-            let setter = Rc::new(move |new| root2.borrow_mut().set(id, new));
-            UseSetInner {
-                _root: root,
-                setter,
-            }
-        },
-        |f| {
-            //
-            &f.setter
-        },
-    )
+    let hook = cx.use_hook(|_| {
+        let id = f.unique_id();
+        let root = root.clone();
+        let root2 = root.clone();
+        let setter = Rc::new(move |new| root2.borrow_mut().set(id, new));
+        UseSetInner {
+            _root: root,
+            setter,
+        }
+    });
+    &hook.setter
 }
 struct UseSetInner<T> {
     _root: Rc<RefCell<AtomRoot>>,
