802c870f00da0905d09cccf0eed0b373d7dbb003
t1m0t <t1m95.tb@gmail.com>
2022-01-21 12:07:50 +0100

    fix

--- src/hooks.rs
+++ src/hooks.rs
@@ -14,7 +14,7 @@ pub fn use_init_atom_root(cx: &ScopeState) -> &Rc<AtomRoot> {
 pub fn use_atom_root(cx: &ScopeState) -> &Rc<AtomRoot> {
     cx.use_hook(|_| match cx.consume_context::<AtomRoot>() {
         Some(root) => root,
-        None => cx.provide_root_context(AtomRoot::new(cx.schedule_update_any())),
+        None => cx.provide_context(AtomRoot::new(cx.schedule_update_any())),
     })
 }
 
